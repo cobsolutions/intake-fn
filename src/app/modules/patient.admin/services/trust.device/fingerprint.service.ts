d5ef06c8f91f5cded6b61df40a6e91f1514a490d
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-10-30 15:12:23 +0300

    Retry getting location in case of time out

--- src/app/modules/patient.admin/services/trust.device/fingerprint.service.ts
+++ src/app/modules/patient.admin/services/trust.device/fingerprint.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { GeolocationService } from '@ng-web-apis/geolocation';
-import { catchError, debounceTime, filter, forkJoin, from, map, Observable, retry, switchMap, take, tap, throwError, timeout } from 'rxjs';
+import { catchError, debounceTime, delay, filter, forkJoin, from, map, Observable, of, retry, retryWhen, scan, switchMap, take, tap, throwError, timeout } from 'rxjs';
 import * as FingerprintJS from '@fingerprintjs/fingerprintjs';
 import { HttpErrorResponse } from '@angular/common/http';
 @Injectable({
@@ -10,14 +10,30 @@ export class FingerprintService {
 
   constructor(private geolocation$: GeolocationService) { }
   public get(): Observable<any[]> {
-    const _callLocation = this.getLocation();
+    const _callLocation = this.getLocation().pipe(
+      retryWhen((errors) =>
+          errors.pipe(
+            scan((retryCount, error) => {
+              if (retryCount >= 2) {
+                throw error;
+              }
+              console.warn(`Retrying... (${retryCount + 1})`);
+              return retryCount + 1;
+            }, 0),
+            delay(2000) // Delay between retries
+          )
+        ),
+        catchError((error) => {
+          console.error('Location retrieval failed:', error);
+          return of(undefined); // Return undefined on failure
+        })
+    );
     const _callDeviceId = this.getDeviceId();
     return forkJoin([_callLocation,_callDeviceId])
   }
 
   private getLocation(): Observable<any> {    
     return this.geolocation$.pipe(
-      timeout(5000),
       take(1),
       catchError(this.handleHttpError));
   }

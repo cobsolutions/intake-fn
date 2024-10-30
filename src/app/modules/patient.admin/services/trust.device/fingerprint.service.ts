e7a90a00f73505ce07716aba790b48fa17c78a91
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-10-30 14:50:12 +0300

    increase time out of getLocation function

--- src/app/modules/patient.admin/services/trust.device/fingerprint.service.ts
+++ src/app/modules/patient.admin/services/trust.device/fingerprint.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 import { GeolocationService } from '@ng-web-apis/geolocation';
-import { filter, forkJoin, from, map, Observable, switchMap, take, tap } from 'rxjs';
+import { catchError, debounceTime, filter, forkJoin, from, map, Observable, retry, switchMap, take, tap, throwError, timeout } from 'rxjs';
 import * as FingerprintJS from '@fingerprintjs/fingerprintjs';
+import { HttpErrorResponse } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class FingerprintService {
 
-  constructor(private readonly geolocation$: GeolocationService) { }
+  constructor(private geolocation$: GeolocationService) { }
   public get(): Observable<any[]> {
     const _callLocation = this.getLocation();
     const _callDeviceId = this.getDeviceId();
     return forkJoin([_callLocation,_callDeviceId])
   }
 
-  private getLocation(): Observable<any> {
+  private getLocation(): Observable<any> {    
     return this.geolocation$.pipe(
-      take(1));
+      timeout(5000),
+      take(1),
+      catchError(this.handleHttpError));
+  }
+  private handleHttpError(error: HttpErrorResponse) {
+    console.log(error)
+    return throwError(() => error);
   }
   private getDeviceId(): Observable<any> {
     return from(FingerprintJS.load()).pipe(

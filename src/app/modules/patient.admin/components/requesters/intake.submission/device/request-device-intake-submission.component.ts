c8c7b7aea8f3e7a2bd1c17647d77e22d67601a72
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-11-14 21:23:11 +0200

    create qr code based on selection of clinic

--- src/app/modules/patient.admin/components/requesters/intake.submission/device/request-device-intake-submission.component.ts
+++ src/app/modules/patient.admin/components/requesters/intake.submission/device/request-device-intake-submission.component.ts
@@ -8,8 +8,11 @@ import { ClinicService } from 'src/app/modules/patient.admin/services/clinic/cli
   styleUrls: ['./request-device-intake-submission.component.css']
 })
 export class RequestDeviceIntakeSubmissionComponent implements OnInit {
-
+  public baseURL: string = location.origin;
   clinics$: Observable<any>
+  selectedClinicUUID: string | undefined = undefined
+  submissionURL: string
+  isGenerated: boolean = false;
   constructor(private clinicService: ClinicService) { }
 
   ngOnInit(): void {
@@ -19,6 +22,10 @@ export class RequestDeviceIntakeSubmissionComponent implements OnInit {
     this.clinics$ = this.clinicService.get().pipe(
       map(result => result.body)
     )
+  }
+  generateQRCode() {
+    this.isGenerated = true
+    this.submissionURL = this.baseURL + '/digital-intake/create?clinicId=' + this.selectedClinicUUID;
   }
 
 }

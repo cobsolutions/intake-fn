e3a18b55c98e9beaa787b265e3c856b314f8fe6a
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-11-25 14:16:31 +0200

    add next button to bi component and pass for and stepper to bio component

--- src/app/modules/patient.digital.intake/components/patient.biometric.identification/patient-biometric-identification.component.ts
+++ src/app/modules/patient.digital.intake/components/patient.biometric.identification/patient-biometric-identification.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'patient-biometric-identification',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-biometric-identification.component.css']
 })
 export class PatientBiometricIdentificationComponent implements OnInit {
-
+  @Input() form: FormGroup;
+  @Input() stepper: MatStepper
+  isValidForm: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  next(){
+    if (this.form.get('bio')?.valid) {
+      this.stepper.next();
+      this.isValidForm = false;
+    } else {
+      this.isValidForm = true;
+    }
+  }
 }

49fa2a5d8dc4fda26ad8c02bcbf859c46f297651
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-13 21:37:48 +0200

    call patient changes service

--- src/app/modules/patient.admin/components/reports/patient.change.report/chnages-report.component.ts
+++ src/app/modules/patient.admin/components/reports/patient.change.report/chnages-report.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { map, tap } from 'rxjs';
+import { PaginationListTemplate } from 'src/app/modules/common/template/pagination.list.template';
+import { PatientChangesService } from '../../../services/monitor/patient-changes.service';
 
 @Component({
   selector: 'app-chnages-report',
   templateUrl: './chnages-report.component.html',
   styleUrls: ['./chnages-report.component.css']
 })
-export class ChnagesReportComponent implements OnInit {
-exportResult() {
-throw new Error('Method not implemented.');
-}
-
-  constructor() { }
+export class ChnagesReportComponent extends PaginationListTemplate implements OnInit {
+  constructor(private patientChangesService:PatientChangesService) {super(); }
 
   ngOnInit(): void {
+    this.patientChangesService.find(this.apiParams$,1,'tee','Update_Referring_Provider')
+    .pipe(
+      tap((response: any) => {
+        this.totalItems$.next(response.number_of_matching_records);
+        if (response.number_of_records) {
+          this.errorMessage$.next('');
+        }
+        this.retry$.next(false);
+        this.loadingData$.next(false);
+      }),
+      map((response: any) => {
+        return response.records;
+      })
+    ).subscribe(dd=>{
+      console.log(JSON.stringify(dd))
+    })
   }
 
 }

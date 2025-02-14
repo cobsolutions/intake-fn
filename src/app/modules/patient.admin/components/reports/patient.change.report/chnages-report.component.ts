fbe4db25e44831b162c7c521b058758fb4bf12e2
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-14 14:51:46 +0200

    add clinics to search panel of patient changes report

--- src/app/modules/patient.admin/components/reports/patient.change.report/chnages-report.component.ts
+++ src/app/modules/patient.admin/components/reports/patient.change.report/chnages-report.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IColumn } from '@coreui/angular-pro/lib/smart-table/smart-table.type';
 import { map, Observable, switchMap, tap } from 'rxjs';
 import { PaginationListTemplate } from 'src/app/modules/common/template/pagination.list.template';
+import { Clinic } from '../../../models/clinic.model';
 import { PatientChangesRecord } from '../../../models/monitor/patient.changes.record';
 import { ClinicService } from '../../../services/clinic/clinic.service';
 import { PatientChangesService } from '../../../services/monitor/patient-changes.service';
@@ -13,9 +14,11 @@ import { PatientChangesService } from '../../../services/monitor/patient-changes
 })
 export class ChnagesReportComponent extends PaginationListTemplate implements OnInit {
   action: string;
-  patientName: string ='';
+  patientName: string = '';
   searchInputNotValid: boolean = false;
   errorMsg: string;
+  clinics: Clinic[]
+  selectedClinic: number;
   changes: Observable<PatientChangesRecord[]>;
   readonly columns: (string | IColumn)[] = [
     {
@@ -47,31 +50,32 @@ export class ChnagesReportComponent extends PaginationListTemplate implements On
     , private clinicService: ClinicService) { super(); }
 
   ngOnInit(): void {
+    this.clinicService.get().pipe(
+      map(result => result.body)
+    )
+      .subscribe((clinics: any) => {
+        this.clinics = clinics
+        if (this.clinics[0].id !== null)
+          this.selectedClinic = this.clinics[0].id
+      })
     this.initListComponent();
     this.action = '1'
   }
   search() {
-    // if (this.patientName === '' || this.patientName === null) {
-    //   this.searchInputNotValid = true
-    //   this.errorMsg = "Please enter patient name"
-    // } else {
-      
-      this.searchInputNotValid = false
-      this.errorMsg = '';
-      this.changes = this.clinicService.selectedClinic$.pipe(
-        switchMap((clinicId: any) => this.patientChangesService.find(this.apiParams$, clinicId, this.patientName, this.action!)),
-        tap((response: any) => {
-          this.totalItems$.next(response.number_of_matching_records);
-          if (response.number_of_records) {
-            this.errorMessage$.next('');
-          }
-          this.retry$.next(false);
-          this.loadingData$.next(false);
-        }),
-        map((response: any) => {
-          return response.records;
-        })
-      )
-    //}
+    this.searchInputNotValid = false
+    this.errorMsg = '';
+    this.changes = this.patientChangesService.find(this.apiParams$, this.selectedClinic, this.patientName, this.action!).pipe(
+      tap((response: any) => {
+        this.totalItems$.next(response.number_of_matching_records);
+        if (response.number_of_records) {
+          this.errorMessage$.next('');
+        }
+        this.retry$.next(false);
+        this.loadingData$.next(false);
+      }),
+      map((response: any) => {
+        return response.records;
+      })
+    )
   }
 }

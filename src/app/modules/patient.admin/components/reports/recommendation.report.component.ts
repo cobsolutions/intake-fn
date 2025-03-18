489081a5fe3ba57268ea73c937cee3e9f54ecc8a
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-18 21:29:43 +0200

    Add Referring Doctor into Source List incase Referring Provider is selected from Patient Source list

--- src/app/modules/patient.admin/components/reports/recommendation.report.component.ts
+++ src/app/modules/patient.admin/components/reports/recommendation.report.component.ts
@@ -169,6 +169,7 @@ export class RecommendationReportComponent implements OnInit {
         this.patientSearchCriteria.clinicId = clinicId;
       })
     this.patientSources = this.patientSources.map((source: any) => ({ ...source, selected: true }));
+    if(this.patientSearchCriteria.sourceType ==='direct')
     this.patientSources = this.patientSources.filter(source => source.entityValue !== 'referringDoctor')
     this.result = {
       resultCount: 0,

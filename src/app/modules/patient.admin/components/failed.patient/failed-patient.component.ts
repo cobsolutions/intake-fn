894c506e6589c3b13c6c628d0fda5b8c62fd66f4
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-07-21 17:09:56 +0300

    add pagination table in failed patient component

--- src/app/modules/patient.admin/components/failed.patient/failed-patient.component.ts
+++ src/app/modules/patient.admin/components/failed.patient/failed-patient.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { IColumn } from '@coreui/angular-pro/lib/smart-table/smart-table.type';
+import { Observable } from 'rxjs';
+import { PaginationListTemplate } from 'src/app/modules/common/template/pagination.list.template';
 
 @Component({
   selector: 'app-failed-patient',
   templateUrl: './failed-patient.component.html',
   styleUrls: ['./failed-patient.component.css']
 })
-export class FailedPatientComponent implements OnInit {
-
-  constructor() { }
+export class FailedPatientComponent extends PaginationListTemplate implements OnInit {
+  //create model for failed pateint model
+  patientData$!: Observable<any[]>;
+  readonly columns: (string | IColumn)[] = [
+    {
+      key: 'patientName',
+      label: 'Patient Name'
+    },
+    {
+      key: 'email',
+      label: 'Email'
+    },
+    {
+      key: 'phoneNumber',
+      label: 'Phone Number'
+    },
+    {
+      key: 'gender',
+      label: 'Gender'
+    }
+  ];
+  constructor() { super();}
 
   ngOnInit(): void {
   }

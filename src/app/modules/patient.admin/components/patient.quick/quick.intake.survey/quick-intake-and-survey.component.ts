55b12410fa63121173c4c8e75ef2bab73615b6b6
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-09-11 18:41:44 +0300

    implement patient quick intake and survey skeleton component

---
+++ src/app/modules/patient.admin/components/patient.quick/quick.intake.survey/quick-intake-and-survey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-intake-and-survey',
+  templateUrl: './quick-intake-and-survey.component.html',
+  styleUrls: ['./quick-intake-and-survey.component.css']
+})
+export class QuickIntakeAndSurveyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

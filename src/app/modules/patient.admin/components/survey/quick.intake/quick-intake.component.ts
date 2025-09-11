3b8be686ce9d78d2e9a4cea99fc828cbbd429601
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-09-11 18:15:19 +0300

    design quick intake component

--- src/app/modules/patient.admin/components/survey/quick.intake/quick-intake.component.ts
+++ src/app/modules/patient.admin/components/survey/quick.intake/quick-intake.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+interface IntakeForm {
+  firstName: FormControl<string | null>;
+  middleName: FormControl<string | null>;
+  lastName: FormControl<string | null>;
+  phone: FormControl<string | null>;
+  email: FormControl<string | null>;
+}
 @Component({
   selector: 'quick-intake',
   templateUrl: './quick-intake.component.html',
@@ -7,9 +14,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuickIntakeComponent implements OnInit {
 
-  constructor() { }
+  intakeForm!: FormGroup<IntakeForm>;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.intakeForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      middleName: [''],
+      lastName: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern(/^[0-9]{10,15}$/)]],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+  saveAndStartSurvey(): void {
+    if (this.intakeForm.valid) {
+      console.log('Save & Start Survey:', this.intakeForm.value);
+      // 🚀 Call backend API to create patient & start survey
+    } else {
+      this.intakeForm.markAllAsTouched();
+    }
+  }
+  saveAndStartSurveyLater() {
+
+  }
+  get f() {
+    return this.intakeForm.controls;
   }
 
 }

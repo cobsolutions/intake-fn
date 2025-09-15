c486870c30bc935c58bff717240875b6b88d9da4
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-09-15 14:07:11 +0300

    generate a skeleton for patient survey report and add it to route

---
+++ src/app/modules/patient.admin/components/reports/patient.survey/patientsurvey-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'patientsurvey-report',
+  templateUrl: './patientsurvey-report.component.html',
+  styleUrls: ['./patientsurvey-report.component.css']
+})
+export class PatientsurveyReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
